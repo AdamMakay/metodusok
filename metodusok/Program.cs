0933e31c9ef9b24beb0922335e3c52cb3dd0b177
makay.adam <makay.adam@CAD1-05>
2025-12-12 08:11:04 +0100

    Add project files.

---
+++ metodusok/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace metodusok
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            UdvozloKep();
+            List<string> menupontok = new List<string>();
+            menupontok.Add("Kerület számítása");
+            menupontok.Add("Terület számítása");
+            menupontok.Add("Kilépés");
+            ListazMenu(menupontok);
+            int valasztott = Kivalasztott(menupontok.Count);
+            
+            Console.WriteLine("Vége!");
+        }
+
+        private static void ListazMenu(List<string> menupontok)
+        {
+            //Petra feladata
+            Console.WriteLine("ide jött!");
+            throw new NotImplementedException("Még nincs kész, ezért ne használd!");
+            Console.WriteLine("ide már jut el!");
+        }
+
+        private static void UdvozloKep()
+        {
+            Console.Clear();
+            Console.SetCursorPosition(30, 0);
+            Console.WriteLine("Üdvözlöm a háromszög szuper alkalmazásban!");
+
+        }
+        private static int Kivalasztott(int valasztas)
+        {
+            Console.Write("Adj meg egy számot: ");
+            valasztas = Convert.ToInt32(Console.ReadLine());
+            return valasztas;
+        }
+    }
+}
