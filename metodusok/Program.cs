49eefa4fe1ba5a80a05839a08e5c58a4bef0d7ff
makay.adam <makayadam1@gmail.com>
2025-12-12 08:59:45 +0100

    hahahahahaahahah

--- metodusok/Program.cs
+++ metodusok/Program.cs
@@ -11,16 +11,28 @@
             menupontok.Add("Kilépés");
             ListazMenu(menupontok);
             int valasztott = Kivalasztott(menupontok.Count);
-            
-            Console.WriteLine("Vége!");
+            switch (valasztott)
+            {
+                case 1:
+                    Console.Write("Adja meg a háromszög egyik oldalat: ");
+                    int oldal = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Adj meg egy számot: ");
+                    int magassag_vonal = Convert.ToInt32(Console.ReadLine());
+                    Terulet(oldal,magassag_vonal); //cső Makay! Szervus Szabolcs
+                    break;
+                case 2:Kerulet(); break;
+
+              //  default:
+            //    break;
+            }
+            Console.WriteLine("Vége! Mindennek vége! Mindennek!");
         }
 
         private static void ListazMenu(List<string> menupontok)
         {
-            //Petra feladata
-            Console.WriteLine("ide jött!");
-            throw new NotImplementedException("Még nincs kész, ezért ne használd!");
-            Console.WriteLine("ide már jut el!");
+            Console.WriteLine("[1]" + menupontok[0]);
+            Console.WriteLine("[2]" + menupontok[1]);
+            Console.WriteLine("[3]" + menupontok[2]);
         }
 
         private static void UdvozloKep()
@@ -32,9 +44,18 @@
         }
         private static int Kivalasztott(int valasztas)
         {
-            Console.Write("Adj meg egy számot: ");
+            Console.WriteLine($"Adjon meg egy számot: ");
             valasztas = Convert.ToInt32(Console.ReadLine());
-            return valasztas;
+            return valasztas-1;
+        }
+        private static void Terulet(int oldal,int magassag_vonal)
+        {
+            float eredmeny = oldal * magassag_vonal / 2;
+            Console.WriteLine($"A háromszög területe: {Math.Round(eredmeny,3)}");
+        }
+        private static void Kerulet()
+        {
+            
         }
     }
 }
