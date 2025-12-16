2fd061f271cad210bd26165bebef4788c76f429a
AdamMakay <makayadam1@gmail.com>
2025-12-16 17:07:31 +0100

    kesz(azt hiszem)

--- metodusok/Program.cs
+++ metodusok/Program.cs
@@ -4,58 +4,68 @@
     {
         static void Main(string[] args)
         {
-            UdvozloKep();
-            List<string> menupontok = new List<string>();
-            menupontok.Add("Kerület számítása");
-            menupontok.Add("Terület számítása");
-            menupontok.Add("Kilépés");
-            ListazMenu(menupontok);
-            int valasztott = Kivalasztott(menupontok.Count);
-            switch (valasztott)
+            List<string> list = new List<string>();
+            String szoveg = "Haromszog";
+            Udvozol(szoveg);
+            list.Add("Kerulet");
+            list.Add("Terulet");
+            list.Add("Kilepes");
+            Menu(list);
+            int kivalasztott_menu = Kivalasztott(list.Count);
+            switch (kivalasztott_menu-1)
             {
-                case 1:
-                    Console.Write("Adja meg a háromszög egyik oldalat: ");
-                    int oldal = Convert.ToInt32(Console.ReadLine());
-                    Console.Write("Adj meg egy számot: ");
-                    int magassag_vonal = Convert.ToInt32(Console.ReadLine());
-                    Terulet(oldal,magassag_vonal); //cső Makay! Szervus Szabolcs
+                
+                case 0:
+                    Console.Write("Adjon meg egy oldalt: ");
+                    int A_oldal = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Adjon meg egy oldalt: ");
+                    int B_oldal = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Adjon meg egy oldalt: ");
+                    int C_oldal = Convert.ToInt32(Console.ReadLine());
+                    Kerulet(A_oldal,B_oldal,C_oldal);
                     break;
-                case 2:Kerulet(); break;
-
-              //  default:
-            //    break;
+                
+                case 1:
+                    Console.Write("Adjon meg egy oldalt: ");
+                    int oldal = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Adja meg az oldalnak a magassag vonalat: ");
+                    int oldal_magassagv = Convert.ToInt32(Console.ReadLine());
+                    Terulet(oldal,oldal_magassagv);
+                    break;
+                
+                case 2:
+                    Console.WriteLine("VEGE");
+                    break;
+                default:
+                    break;
             }
-            Console.WriteLine("Vége! Mindennek vége! Mindennek!");
+        }
+        static void Udvozol(String szoveg)
+        {
+            int Con_width = Console.WindowWidth;
+            Console.SetCursorPosition((Con_width - szoveg.Length) / 2, 0);
+            Console.WriteLine(szoveg);
         }
 
-        private static void ListazMenu(List<string> menupontok)
+        static void Menu(List<string> lista)
         {
-            Console.WriteLine("[1]" + menupontok[0]);
-            Console.WriteLine("[2]" + menupontok[1]);
-            Console.WriteLine("[3]" + menupontok[2]);
+            Console.WriteLine("[1]" + lista[0]);
+            Console.WriteLine("[2]" + lista[1]);
+            Console.WriteLine("[3]" + lista[2]);
+        }
+        static int Kivalasztott(int szam)
+        {
+            int valaszt = Convert.ToInt32(Console.ReadLine());
+            return valaszt;
+        }
+        static void Kerulet(int a, int b, int c)
+        {
+           Console.WriteLine(a+b+c);
+        }
+        static void Terulet(int a, int b)
+        {
+            Console.WriteLine(Convert.ToDouble(a*b/2));
         }
 
-        private static void UdvozloKep()
-        {
-            Console.Clear();
-            Console.SetCursorPosition(30, 0);
-            Console.WriteLine("Üdvözlöm a háromszög szuper alkalmazásban!");
-
-        }
-        private static int Kivalasztott(int valasztas)
-        {
-            Console.WriteLine($"Adjon meg egy számot: ");
-            valasztas = Convert.ToInt32(Console.ReadLine());
-            return valasztas-1;
-        }
-        private static void Terulet(int oldal,int magassag_vonal)
-        {
-            float eredmeny = oldal * magassag_vonal / 2;
-            Console.WriteLine($"A háromszög területe: {Math.Round(eredmeny,3)}");
-        }
-        private static void Kerulet()
-        {
-            
-        }
     }
 }
